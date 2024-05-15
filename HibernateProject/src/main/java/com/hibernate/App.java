5430042479394ce3a60710a9799d1aba6cf4526d
Abhishek Pardhiya <LENOVO@192.168.1.41>
2024-05-15 16:12:04 +0530

    test

--- HibernateProject/src/main/java/com/hibernate/App.java
+++ HibernateProject/src/main/java/com/hibernate/App.java
@@ -19,24 +19,29 @@ public class App {
 		SessionFactory sessionFactory = new Configuration().configure("com/hibernate/hibernate.cfg.xml")
 				.buildSessionFactory();
 
-		
+		//Open session using session Factory object 
 		Session session = sessionFactory.openSession();
 
+		//Create an object of Entity class
 		Employee employee = new Employee();
 		employee.setEmployeeName("John Doe");
 		employee.setEmployeeAddress("Texas");
 
 		try {
+			//Begin the transaction
 			Transaction tx = session.beginTransaction();
 
-			session.save(employee);
+			//save the object using the save() method of session object
+ 			session.save(employee);
 
+ 			//Commit all the changes
 			tx.commit();
 		}
 
 		catch (Exception e) {
 			e.printStackTrace();
 		} finally {
+			//Closing the session after transaction completed
 			session.close();
 		}
 	}
